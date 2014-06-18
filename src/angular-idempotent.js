fb68d96c8a1bc1a5dabd3f4d12a3ed66e60f8fe1
Pedro Del Gallego <pedro.delgallego@gmail.com>
2014-06-18 22:31:01 +0200

    refacto: move files to src add

---
+++ src/angular-idempotent.js
@@ -0,0 +1,20 @@
+(function(window, angular, undefined){
+  'use strict';
+
+  angular.module('ngIdempotent', [])
+    .factory('$idempotent', ['$http', '$q', '$timeout', function($http, $q, $timeout) {
+      // var $resourceMinErr = angular.$$minErr('$idempotent');
+      var tracker = {};
+
+      var ngIdempotent = {
+        generateUUID: function () {
+          return 'xxxxxxxx-xxxx-yxxx-yxxx-xxxxxxxxxxxx-yyyy-yyyyyy'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+          });
+        }
+      };
+
+      return ngIdempotent;
+    }]);
+}(window, window.angular))
